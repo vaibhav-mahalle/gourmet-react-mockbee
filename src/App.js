5dfb0e70320f372ef7741c68cbb506a0fd1de368
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-04-08 09:07:13 +0530

    feat: added header and footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import logo from "./logo.png";
 import RoutingPath from "./configs";
+import { Header, Footer} from "./components";
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <RoutingPath/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <div>
+            <h1>Download Our App</h1>
+            <div>Made with Love by Vaibhav Mahalle.</div>
+
+        </div>
+    )
+}
+
+export default Footer;
