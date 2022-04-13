395d803dd1f36e8b2fdfedd7273eab7045bca587
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-04-14 01:45:16 +0530

    feat: fetched the products using axios

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,46 @@
+import { Filter, ProductCard } from "../components";
+// import { products } from "../backend/db/products";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Product = () => {
-    return(
-        <div class="Page-container">This is Product</div>
-    )
-}
+  const [productData, setProductData] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get("/api/products");
+      setProductData(res.data.products);
+    } catch (e) {
+      alert("there is some error!!");
+    }
+  };
+
+  useEffect(() => fetchProducts(), []);
+
+  return (
+    <div className="Page-container">
+      <div className="product-listing-container">
+        <Filter />
+        <div className="product-container"> 
+        {productData.map((item) => {
+          return (
+            <ProductCard
+              id={item.id}
+              title={item.id}
+              isLiked={item.isLiked}
+              imageUrl={item.imageUrl}
+              qty={item.quantity}
+              price={item.price}
+              rating={item.rating}
+              storeName={item.storeName}
+              category={item.category}
+            />
+          );
+        })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
