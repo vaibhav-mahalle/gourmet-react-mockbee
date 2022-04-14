f446c8d33a2077aecd1147d08d3556d4478d2b2d
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-04-15 00:45:59 +0530

    feat: created filter context

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -1,7 +1,14 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, {createContext, useContext, useReducer } from "react";
+import { filterReducerFunction } from "../reducer/filterReducer";
 
 const FilterContext = createContext();
 
+const FilterProvider = ({children}) => {
+    const [filterState,filterDispatch] = useReducer(filterReducerFunction, {sortBy:"",isIndian:false,isChinese:false,isDessert:false,isFastFood:false,rating:5});
+    return <FilterContext.Provider value={{filterState,filterDispatch}}>
+        {children}
+    </FilterContext.Provider>
+}
 
 
 const useFilter = () => useContext(FilterContext);
