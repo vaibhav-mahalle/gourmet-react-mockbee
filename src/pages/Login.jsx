fc769810d0a4d87f826fa29b8a767e1366f9b8fe
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-04-17 17:10:26 +0530

    feat: added login and signup pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return(
-        <div className="Page-container">This is Login</div>
-    )
-}
+  return (
+    <div className="Page-container">
+      
+      <div className="page-container flex-center">
+        <div className="login-card p-1 flex-column flex-center">
+          <div className="font-md p-b-1">
+            Login
+            <span className="font-sm">
+              or
+              <Link to="/Signup" className href="/pages/signup.html">
+                create an account
+              </Link>
+            </span>
+          </div>
+          <div className="p-b-1">
+            <input className="input" type="text" placeholder="Phone Number" />
+          </div>
+          <div>
+            <button className="btn-primary ">Login</button>
+          </div>
+          <div className="p-l-1">
+            I accept the <Link to="/Login">Terms &amp; Conditions</Link> &amp;
+            <Link to="/Login">Privacy Policy</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
